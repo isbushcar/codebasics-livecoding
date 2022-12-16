d26aa608d244567777839b4c2cd9002f69927eae
Andrey Berezin <64121656+isbushcar@users.noreply.github.com>
2022-12-16 19:52:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+import requests
+
+
 EXCHANGE_RATES = {
     'USD': 1,
     'EUR': 1.3,
     'RUB': 0.01425,
     'BTC': 1000,
+    'GBP': 1.5,
 }
 
 
-def get_exchange_rates():  # TODO: получать данные по API
+def get_exchange_rates():  # Реализовать получение курсов по API
     return EXCHANGE_RATES
 
 
 def main():
     exchange_rates = get_exchange_rates()
-    base_currency = input(f'Введите валюту, которую хотите менять {list(exchange_rates)}\n')  # TODO: сделать красивый вывод списка
-    target_currency = input(f'Введите валюту, на которую хотите менять {list(exchange_rates)}\n')  # TODO: сделать красивый вывод списка
-    # TODO: проверить ввод пользователя на то, что валюта существует
-    currency_amount = int(input('Введите количество валюты\n'))  # TODO: добавить возможность использования нецелых чисел
+    base_currency = input(f'Введите валюту, которую хотите обменять {list(exchange_rates)}\n')
+    # Исправить отображение списка валют
+    # Проверять, действительную ли валюту ввел пользователь и отрицательные значения
+    # Обрезать лишние пробелы при вводе, проверять регистр ввода
+    target_currency = input(f'Введите валюту, на которую хотите обменять {list(exchange_rates)}\n')
+    # Исправить отображение списка валют
+    # Проверять, действительную ли валюту ввел пользователь и отрицательные значения
+    # Обрезать лишние пробелы при вводе, проверять регистр ввода
+    currency_amount = float(input('Введите количество валюты\n'))
 
     cross_course = exchange_rates[base_currency] / exchange_rates[target_currency]
-
-    print(round(currency_amount * cross_course, 2))
+    result = round(cross_course * currency_amount, 2)
+    print(result)  # добавить валюту в вывод
 
 
 main()
+
